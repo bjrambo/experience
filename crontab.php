3501455ae7dfa14357949c3772407d1ce1995702
BJRambo <qw5414@naver.com>
2021-02-01 19:16:56 +0900

    콘솔에서 PHP실행하지 않는 경우 실행하지 않도록 함.

--- crontab.php
+++ crontab.php
@@ -1,4 +1,10 @@
 <?php
+// Abort if not CLI.
+if (PHP_SAPI !== 'cli')
+{
+	echo "This script must be executed on the command line interface.\n";
+	exit(1);
+}
 
 define('__XE__', true);
 require_once('../../config/config.inc.php'); //XE config.inc.php 주소
